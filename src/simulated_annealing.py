65e733e3ca22343c0c34021ee0ece6f3d2e4fc93
ACharchula <antoni.charchula@gmail.com>
2019-05-05 14:37:30 +0200

    assigned neighbours for simanneal

--- src/simulated_annealing.py
+++ src/simulated_annealing.py
@@ -1,6 +1,7 @@
 from simanneal import Annealer
 import random
 
+
 class SimulatedAnnealing(Annealer):
 
     def __init__(self, players_and_scores):
@@ -10,12 +11,12 @@ class SimulatedAnnealing(Annealer):
 
     def move(self):
         player = random.randint(0, 2)
-        #EHHHH...
+        # EHHHH...
 
     def energy(self):
-        score = self.state[len(self.state)-1]
+        score = self.state[len(self.state) - 1]
         return 20 - score
-        #20 is a random number, the state will change if energy decrease
+        # 20 is a random number, the state will change if energy decrease
 
 
 class Player:
@@ -25,4 +26,52 @@ class Player:
         self.shot = shot
         self.finishing = finishing
 
-    #def find_neighbour(self, players_and_scores):
+    def __eq__(self, other):
+        if other.overall == self.overall and other.shot == self.shot and other.finishing == self.finishing:
+            return True
+        else:
+            return False
+
+
+class PlayersAndScore:
+
+    def __init__(self, key, value):
+        self.player1 = Player(key[0], key[1], key[2])
+        self.player2 = Player(key[3], key[4], key[5])
+        self.player3 = Player(key[6], key[7], key[8])
+        self.score = value
+        self.neighbours = list()
+
+    def add_neighbour(self, neighbour):
+        self.neighbours.append(neighbour)
+
+    def check_if_contains_a_player(self, player):
+        if player == self.player1 or player == self.player2 or player == self.player3:
+            return True
+        else:
+            return False
+
+
+class AllData:
+
+    def __init__(self, players_scores):
+        self.players_and_scores = dict()
+
+        for k, v in players_scores.items():
+            self.players_and_scores[k] = PlayersAndScore(k, v)
+
+        self.assign_neighbours()
+
+    #neighbour is a row with at least one the same player among attackers
+    def assign_neighbours(self):
+        count = 0
+        for k1, v1 in self.players_and_scores.items():
+            print(count)
+            for k2, v2 in self.players_and_scores.items():
+                if k1 == k2:
+                    continue
+
+                if v2.check_if_contains_a_player(v1.player1) or v2.check_if_contains_a_player(v1.player2) or v2.check_if_contains_a_player(v1.player3):
+                    v1.add_neighbour(v2)
+
+            count += 1
--- src/test.py
+++ src/test.py
@@ -1,1 +1,21 @@
-print("hello")
+import csv
+
+from src.simulated_annealing import *
+
+players_and_scores = dict()
+
+with open('players_with_avg_goals.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    count = 0
+    for row in csv_reader:
+        if count == 0:
+            count += 1
+            continue
+        else:
+            key = (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8])
+            players_and_scores[key] = row[9]
+            count += 1
+
+print(f'Processed {count} lines.')
+
+data = AllData(players_and_scores)
