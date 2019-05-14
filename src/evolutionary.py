e331deddc47a6081b7d67dcb0e1b646b932e4209
tokerson <kuba.tokarzewski@gmail.com>
2019-05-14 23:00:58 +0200

    added comments describing the algorithm

--- src/evolutionary.py
+++ src/evolutionary.py
@@ -8,19 +8,22 @@ from structures import *
 
 data = AllData.load_data_from_file("przestrzen.txt")
 
-
+#function used to generate random indices of class Player
 def create_player():
     return Player(random.randint(64, 99), random.randint(64, 99), random.randint(64, 99))
 
 
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMax)
+creator.create("FitnessMax", base.Fitness, weights=(1.0,)) #weights is 1.0 because we want to maximize it
+creator.create("Individual", list, fitness=creator.FitnessMax) #our individual(chromosome) is a list of 3 players
+#during crossover we will cross two individuals and if one doesn't exist in the search space then it's score will be 0
 
 toolbox = base.Toolbox()
+#define one gene (index) to be a player returned by create_player method
 toolbox.register("indices", create_player)
+#define individual to call indices function (create_player) 3 times
 toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.indices, n=3)
 
-ind = toolbox.individual()
+ind = toolbox.individual()  # [ Player1, Player2, Player3 ]
 
 print(ind[0])
 print("\n")
