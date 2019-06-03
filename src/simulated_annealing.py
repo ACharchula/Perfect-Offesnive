64d56332924922dcea8270b1bd5f0f35e8f5bbc3
ACharchula <antoni.charchula@gmail.com>
2019-06-03 16:18:57 +0200

    testing simanneal

--- src/simulated_annealing.py
+++ src/simulated_annealing.py
@@ -5,9 +5,11 @@ import random
 
 class SimulatedAnnealing(Annealer):
 
-    def __init__(self, state, data):
+    def __init__(self, state, data, value_to_find, max_price):
         self.data = data
+        self.value_to_find = value_to_find
         self.state = state
+        self.max_price = max_price
         super(SimulatedAnnealing, self).__init__(self.state)
 
     def move(self):
@@ -16,14 +18,21 @@ class SimulatedAnnealing(Annealer):
 
     def energy(self):
         score = self.data.players_and_scores[self.state].score
-        if score == 13:
+
+        cost = (int(self.state[0]) + int(self.state[3]) + int(self.state[6]))/3
+        if cost > self.max_price:
+            return cost
+
+        if score == self.value_to_find:
             self.steps = 0
+            return 0
+
         return 20 - score
         # 20 is a random number, the state will change if energy decrease
 
 
-def perform_simulated_annealing(start_key, data, steps):
-    sa = SimulatedAnnealing(start_key, data)
+def perform_simulated_annealing(start_key, data, steps, value_to_find, max_price):
+    sa = SimulatedAnnealing(start_key, data, value_to_find, max_price)
     sa.steps = steps
     sa.copy_strategy = "slice"
     print('===Simulated Annealing===')
@@ -36,3 +45,20 @@ def perform_simulated_annealing(start_key, data, steps):
     print(data.players_and_scores[state].player3)
     print(data.players_and_scores[state].score)
     print('=========================')
+
+def perform_simulated_annealing_test(start_key, data, steps, value_to_find, max_price, Tmax, Tmin):
+    sa = SimulatedAnnealing(start_key, data, value_to_find, max_price)
+    sa.steps = steps
+
+    if Tmax != -1:
+        sa.Tmax = Tmax
+
+    if Tmin != -1:
+        sa.Tmin = Tmin
+
+    sa.updates = 0
+    sa.copy_strategy = "slice"
+    start = time.time()
+    state, e = sa.anneal()
+    end = time.time()
+    return end - start, data.players_and_scores[state].score
--- src/test.py
+++ src/test.py
@@ -6,6 +6,7 @@ from src.structures import AllData
 
 players_and_scores = dict()
 
+
 def create_dict_from_file(filename):
     result = dict()
 
@@ -17,11 +18,13 @@ def create_dict_from_file(filename):
 
     return result
 
+
 def create_search_space_for_goals():
     players_and_scores = create_dict_from_file('players_with_avg_goals_2.csv')
     data = AllData(players_and_scores)
     data.assign_neighbours()
     data.save_data_to_file("przestrzen.txt")
+
 
 def create_search_space_for_shotons():
     players_and_scores = create_dict_from_file('players_with_avg_shotons_2.csv')
@@ -30,17 +33,59 @@ def create_search_space_for_shotons():
     data.save_data_to_file("przestrzen2.txt")
 
 
-def simulated_annealing(shotons):
-    key = ('82', '75', '76', '78', '82', '78', '74', '81', '74')
-
+def simulated_annealing(shotons, value_to_find, max_price, steps, test, Tmax, Tmin):
     if shotons:
+        key = random.choice(list(create_dict_from_file('players_with_avg_goals_2.csv').keys()))
         data = AllData.load_data_from_file("przestrzen2.txt")
     else:
+        key = random.choice(list(create_dict_from_file('players_with_avg_shotons_2.csv').keys()))
         data = AllData.load_data_from_file("przestrzen.txt")
 
-    perform_simulated_annealing(key, data, 600)
+    # key = ('82', '75', '76', '78', '82', '78', '74', '81', '74')
+    if test:
+        return perform_simulated_annealing_test(key, data, steps, value_to_find, max_price, Tmax, Tmin)
+    else:
+        perform_simulated_annealing(key, data, steps, value_to_find, max_price)
 
-simulated_annealing(False)
-linear_search(create_dict_from_file('players_with_avg_goals_2.csv'))
-simulated_annealing(True)
-linear_search(create_dict_from_file('players_with_avg_shotons_2.csv'))
+    return
+
+
+def test_simanneal():
+    # write_results(False, 7, 100, 30000, 50000.0, 2.5)
+    # write_results(False, 7, 100, 30000, 40000.0, 2.5)
+    # write_results(False, 7, 100, 30000, 30000.0, 2.5)
+    # write_results(False, 7, 100, 30000, -1, -1)
+    write_results(True, 13, 100, 10000, -1, -1)
+
+
+def perform_simanneal(shotons, expected_value, max_cost, steps, Tmax, Tmin):
+    if shotons:
+        linear_result = linear_search_test(create_dict_from_file('players_with_avg_shotons_2.csv'), max_cost)
+    else:
+        linear_result = linear_search_test(create_dict_from_file('players_with_avg_goals_2.csv'), max_cost)
+
+    result = 0
+    errors = 0
+    max_range = 20
+    for i in range(0, max_range):
+        resultSimanneal = simulated_annealing(shotons, expected_value, max_cost, steps, True, Tmax, Tmin)
+        if linear_result[1] != resultSimanneal[1]:
+            errors += 1
+        else:
+            result += resultSimanneal[0]
+
+    return result / (max_range - errors), errors, linear_result[0]
+
+
+def write_results(shotons, expected_value, max_cost, steps, Tmax, Tmin):
+    result = perform_simanneal(shotons, expected_value, max_cost, steps, Tmax, Tmin)
+    print('================================================')
+    print('PARAMS ' + 'shotons: ' + str(shotons) + ' max_cost: ' + str(expected_value) + ' max_cost: ' + str(
+        max_cost) + ' steps: ' + str(steps)
+          + ' Tmax: ' + str(Tmax) + ' Tmin: ' + str(Tmin))
+    print('simulated annealing speed: ' + str(result[0]))
+    print('errors: ' + str(result[1]))
+    print('linear speed: ' + str(result[2]))
+
+
+test_simanneal()
