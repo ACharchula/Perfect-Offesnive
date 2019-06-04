b8fe4cc122f05061907802b5d7e6f1b29ebb74df
tokerson <kuba.tokarzewski@gmail.com>
2019-06-04 19:42:01 +0200

    evoultionary alg tests written

--- src/evolutionary.py
+++ src/evolutionary.py
@@ -1,104 +1,120 @@
 from deap import base
 from deap import creator
 from deap import tools
-
+from enum import Enum
 from src.structures import *
 
-data = AllData.load_data_from_file("przestrzen2.txt")
+
+class Data(Enum):
+    GOALS = "przestrzen.txt"
+    SHOTONS = "przestrzen2.txt"
 
 
-def evaluate(individual, max_price):
-    score = data.get_score(individual)
-    cost = (int(individual[0]) + int(individual[3]) + int(individual[6])) / 3
-    if cost < max_price:
-        return score,
-
-    return 0.0,
-    # return data.get_score(individual),
+class Selection(Enum):
+    # ROULETTE = tools.selRoulette
+    TOURNAMENT = tools.selTournament
+    BEST = tools.selBest
 
 
-# crossover is about swapping last players of two threes
-def cross_over(individual1, individual2):
-    return creator.Individual(individual1[:6] + individual2[6:10]), creator.Individual(
-        individual2[:6] + individual1[6:10])
+class Evo:
 
+    def __init__(self, goal, selection, tournSize=None):
+        self.data = AllData.load_data_from_file(goal.value)
 
-# we randomly choose if we mutate or not. If we do we randomly pick a neighbor (if one exists) of the individual
-def mutate(individual):
-    return creator.Individual(data.get_random_neighbor(individual))
+        creator.create("FitnessMax", base.Fitness, weights=(1.0,))  # weights is 1.0 because we want to maximize it
+        creator.create("Individual", tuple,
+                       fitness=creator.FitnessMax)  # our individual(chromosome) is a list of 3 players
+        # during crossover we will cross two individuals and if one doesn't exist in the search space then it's score will be 0
 
+        self.toolbox = base.Toolbox()
+        # define one gene (index) to be a player returned by create_player method
+        # toolbox.register("indices", create_player)
+        # define individual to call indices function (create_player) 3 times
+        self.toolbox.register("individual", self.init_individual, creator.Individual)
 
-def init_individual(ind_class):
-    return ind_class(data.get_random_players())
-    # return ind_class(create_player() + create_player() + create_player())
-
-
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))  # weights is 1.0 because we want to maximize it
-creator.create("Individual", tuple, fitness=creator.FitnessMax)  # our individual(chromosome) is a list of 3 players
-# during crossover we will cross two individuals and if one doesn't exist in the search space then it's score will be 0
-
-toolbox = base.Toolbox()
-# define one gene (index) to be a player returned by create_player method
-# toolbox.register("indices", create_player)
-# define individual to call indices function (create_player) 3 times
-toolbox.register("individual", init_individual, creator.Individual)
-
-toolbox.register("evaluate", evaluate)
-toolbox.register("mate", cross_over)
-toolbox.register("mutate", mutate)
-toolbox.register("select", tools.selBest)
-
-# defines a population to be a list of individuals
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-
-
-def run_evolutionary_algorithm(crossover_possibility, mutation_possibility, population_size, max_price, max_stagnation, wanted_value):
-    pop = toolbox.population(n=population_size)
-
-    for ind in pop:
-        ind.fitness.values = toolbox.evaluate(ind, max_price)
-
-    CXPB = crossover_possibility
-    MUTPB = mutation_possibility
-
-    g = 0
-    didnt_change = 0
-    curr_best = tools.selBest(pop, 1)[0]
-    fits = [ind.fitness.values[0] for ind in pop]
-    if wanted_value is None:
-        wanted_value = -1
-
-
-    while didnt_change < max_stagnation:# and max(fits) >= wanted_value:
-        g = g + 1
-        # Select the next generation individuals
-        offspring = toolbox.select(pop, len(pop))
-        # Clone the selected individuals
-        # offspring = list(map(toolbox.clone, offspring))
-
-        for i in range(0, len(offspring) - 1, 2):
-            if random.random() < CXPB:
-                offspring[i], offspring[i + 1] = toolbox.mate(offspring[i], offspring[i + 1])
-
-        for i in range(len(offspring)):
-            if random.random() < MUTPB:
-                offspring[i] = toolbox.mutate(offspring[i])
-
-        best_ind = tools.selBest(pop, 1)[0]
-        # we make sure to take the best element from previous population to the next population
-        pop = toolbox.select([best_ind] + offspring, len(pop))
-
-        # increasing counter if the best element remains the same
-        if best_ind == curr_best:
-            didnt_change = didnt_change + 1
+        self.toolbox.register("evaluate", self.evaluate)
+        self.toolbox.register("mate", self.cross_over)
+        self.toolbox.register("mutate", self.mutate)
+        if selection is Selection.TOURNAMENT:
+            self.toolbox.register("select", selection, tournsize=tournSize)
         else:
-            didnt_change = 0
-            curr_best = best_ind
+            self.toolbox.register("select", selection)
+
+        # defines a population to be a list of individuals
+        self.toolbox.register("population", tools.initRepeat, list, self.toolbox.individual)
+
+    # crossover is about swapping last players of two threes
+    def evaluate(self, individual, max_price=99):
+        score = self.data.get_score(individual)
+        cost = (int(individual[0]) + int(individual[3]) + int(individual[6])) / 3
+        if cost < max_price:
+            return score,
+
+        return 0.0,
+
+    def cross_over(self, individual1, individual2):
+        return creator.Individual(individual1[:6] + individual2[6:10]), creator.Individual(
+            individual2[:6] + individual1[6:10])
+
+    def mutate(self, individual):
+        return creator.Individual(self.data.get_random_neighbor(individual))
+
+    def init_individual(self, ind_class):
+        return ind_class(self.data.get_random_players())
+        # return ind_class(create_player() + create_player() + create_player())
+
+    # we randomly choose if we mutate or not. If we do we randomly pick a neighbor (if one exists) of the individual
+
+    def run_evolutionary_algorithm(self, crossover_possibility, mutation_possibility, population_size, max_price,
+                                   max_stagnation, wanted_value):
+        pop = self.toolbox.population(n=population_size)
 
         for ind in pop:
-            ind.fitness.values = toolbox.evaluate(ind, max_price)
+            ind.fitness.values = self.toolbox.evaluate(ind, max_price)
 
+        CXPB = crossover_possibility
+        MUTPB = mutation_possibility
+
+        g = 0
+        didnt_change = 0
+        curr_best = tools.selBest(pop, 1)[0]
         fits = [ind.fitness.values[0] for ind in pop]
+        if wanted_value is None:
+            wanted_value = -1
 
-    best_ind = tools.selBest(pop, 1)[0]
-    return g, best_ind, best_ind.fitness.values
+        # while didnt_change < max_stagnation:  # and max(fits) >= wanted_value:
+        while didnt_change < max_stagnation and max(fits) < wanted_value:
+            g = g + 1
+            # Select the next generation individuals
+            offspring = self.toolbox.select(pop, len(pop))
+            # Clone the selected individuals
+            # offspring = list(map(toolbox.clone, offspring))
+
+            for i in range(0, len(offspring) - 1, 2):
+                if random.random() < CXPB:
+                    offspring[i], offspring[i + 1] = self.toolbox.mate(offspring[i], offspring[i + 1])
+
+            for i in range(len(offspring)):
+                if random.random() < MUTPB:
+                    offspring[i] = self.toolbox.mutate(offspring[i])
+
+            best_ind = tools.selBest(pop, 1)[0]
+            # we make sure to take the best element from previous population to the next population
+            pop = self.toolbox.select([best_ind] + offspring, len(pop))
+
+            # increasing counter if the best element remains the same
+            if best_ind == curr_best:
+                didnt_change = didnt_change + 1
+            else:
+                didnt_change = 0
+                curr_best = best_ind
+
+            for ind in pop:
+                ind.fitness.values = self.toolbox.evaluate(ind, max_price)
+
+            fits = [ind.fitness.values[0] for ind in pop]
+
+        best_ind = tools.selBest(pop, 1)[0]
+        return g, best_ind, best_ind.fitness.values
+
+
--- src/test.py
+++ src/test.py
@@ -3,7 +3,7 @@ import csv
 from src.simulated_annealing import *
 from src.linear_check import *
 from src.structures import AllData
-from src.evolutionary import run_evolutionary_algorithm
+from src.evolutionary import Evo, Selection, Data
 from prettytable import PrettyTable
 
 players_and_scores = dict()
@@ -92,20 +92,154 @@ def write_results(shotons, expected_value, max_cost, steps, Tmax, Tmin):
 
 # test_simanneal()
 
-x = PrettyTable()
-x.field_names = ["Population size", "Stagnation", "Time", "Generations", "Best Individual", "Score", "Price"]
 
-for i in range(1, 10):
-    pop_size = 6 + i
-    stagnation = 200
-    start = time.time()
-    g, best_ind, score = run_evolutionary_algorithm(0.5, 0.8, pop_size * i, 99, stagnation, 12.0)
-    end = time.time()
 
-    print("Generations %s" % g)
-    print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
-    print("Time is ", end - start)
-    x.add_row([pop_size, stagnation, end - start, g, best_ind, score,
-               (int(best_ind[0]) + int(best_ind[3]) + int(best_ind[6])) / 3])
 
-print(x)
+def run_n_times_and_gather_info(n, expected_result, func_to_run, *args):
+    results = []
+    times = []
+    highest_score = 0
+    good = 0
+    price = 0
+    for i in range(0, n):
+        start = time.time()
+        result = func_to_run(*args)
+        end = time.time()
+
+        if result[2][0] > highest_score:
+            highest_score = result[2][0]
+            price = (int(result[1][0]) + int(result[1][3] )+ int(result[1][6])) / 3
+
+        if result[2][0] == expected_result:
+            good = good + 1
+
+        results.append(result)
+        times.append(end - start)
+
+    sum = 0
+    generations = 0
+    total_time = 0
+    for time_ in times:
+        total_time += time_
+    for elem in results:
+        sum += elem[2][0]
+        generations += elem[0]
+
+    return generations / len(results), sum / len(results), total_time / len(results), highest_score, good / n, price
+
+
+def test_one_set_of_parameters():
+    x = PrettyTable()
+    x.field_names = ["Population size", "Stagnation", "Avg Time", "Avg Generations", "Avg Score", "Highest Score",
+                     "Price", "Repetitions", "Accuracy"]
+
+    evo = Evo(Data.SHOTONS, Selection.BEST)
+    avg_gen, avg_score, avg_time, high_score, accuracy, price = run_n_times_and_gather_info(10, 13,
+                                                                                     evo.run_evolutionary_algorithm,
+                                                                                     0.5, 0.2, 100, 99, 200, 13.0)
+    x.add_row([10, 200, avg_time, avg_gen, avg_score, high_score, price, 10, accuracy])
+    print(x)
+
+
+def test_evolutionary_algorithm_with_changing_population_size(goal, selection, cross_over_prob, mutation_prob, pop_size,
+                                                              max_price, stagnation,
+                                                              expected_result, tournSize=None):
+    print("Pop size= %s; cross_over_probabilty= %s; mutation_propability=%s" %(pop_size,cross_over_prob,mutation_prob))
+    x = PrettyTable()
+    x.field_names = ["Population size", "Stagnation", "Avg Time", "Avg Generations", "Avg Score", "Highest Score",
+                     "Price", "Repetitions", "Accuracy"]
+    n = 10
+
+    evo = Evo(goal, selection, tournSize=tournSize)
+    popSize = pop_size
+    for i in range(1, 10):
+        avg_gen, avg_score, avg_time, high_score, accuracy, price = run_n_times_and_gather_info(n, expected_result,
+                                                                                                evo.run_evolutionary_algorithm,
+                                                                                                cross_over_prob,
+                                                                                                mutation_prob,
+                                                                                                popSize, max_price,
+                                                                                                stagnation,
+                                                                                                expected_result)
+
+        x.add_row([popSize, stagnation, avg_time, avg_gen, avg_score, high_score, price, n, accuracy])
+        popSize = pop_size + i * 10
+    print(x)
+
+def test_evolutionary_algorithm_with_changing_stagnation(goal, selection, cross_over_prob, mutation_prob, pop_size,
+                                                         max_price, stagnation,
+                                                         expected_result, tournSize=None):
+    x = PrettyTable()
+    x.field_names = ["Population size", "Stagnation", "Avg Time", "Avg Generations", "Avg Score", "Highest Score",
+                     "Price", "Repetitions", "Accuracy"]
+    n = 20
+
+    evo = Evo(goal, selection, tournSize=tournSize)
+    stagnation_ = stagnation
+    for i in range(1, 10):
+        avg_gen, avg_score, avg_time, high_score, accuracy, price = run_n_times_and_gather_info(n, expected_result,
+                                                                                         evo.run_evolutionary_algorithm,
+                                                                                         cross_over_prob,
+                                                                                         mutation_prob,
+                                                                                         pop_size, max_price,
+                                                                                         stagnation_,
+                                                                                         expected_result)
+
+        x.add_row([pop_size, stagnation_, avg_time, avg_gen, avg_score, high_score, price, n, accuracy])
+        stagnation_ = stagnation + i * 20
+
+    print(x)
+
+
+def test_evolutionary_algorithm_with_decreasing_cross_over_propability(goal, selection, cross_over_prob, mutation_prob,
+                                                                       pop_size, max_price, stagnation,
+                                                                       expected_result, tournSize=None):
+    x = PrettyTable()
+    x.field_names = ["Population size", "Stagnation", "Avg Time", "Avg Generations", "Avg Score", "Highest Score",
+                     "Price", "Repetitions", "Accuracy", "CRSPB"]
+    n = 20
+
+    evo = Evo(goal, selection, tournSize=tournSize)
+    crspb = cross_over_prob
+    for i in range(1, 10):
+        avg_gen, avg_score, avg_time, high_score, accuracy, price = run_n_times_and_gather_info(n, expected_result,
+                                                                                         evo.run_evolutionary_algorithm,
+                                                                                         crspb,
+                                                                                         mutation_prob,
+                                                                                         pop_size, max_price,
+                                                                                         stagnation,
+                                                                                         expected_result)
+
+        x.add_row([pop_size, stagnation, avg_time, avg_gen, avg_score, high_score, price, n, accuracy, crspb])
+        crspb = crspb - 0.75
+
+    print(x)
+
+
+def test_evolutionary_algorithm_with_decreasing_mutation_propability(goal, selection, cross_over_prob,
+                                                                       mutation_prob, pop_size, max_price,
+                                                                       stagnation,
+                                                                       expected_result, tournSize=None):
+    x = PrettyTable()
+    x.field_names = ["Population size", "Stagnation", "Avg Time", "Avg Generations", "Avg Score", "Highest Score",
+                     "Price", "Repetitions", "Accuracy", "MTNPB"]
+    n = 20
+
+    evo = Evo(goal, selection, tournSize=tournSize)
+    mtnpb = mutation_prob
+    for i in range(1, 10):
+        avg_gen, avg_score, avg_time, high_score, accuracy, price = run_n_times_and_gather_info(n, expected_result,
+                                                                                         evo.run_evolutionary_algorithm,
+                                                                                         cross_over_prob,
+                                                                                         mtnpb,
+                                                                                         pop_size, max_price,
+                                                                                         stagnation,
+                                                                                         expected_result)
+
+        x.add_row([pop_size, stagnation, avg_time, avg_gen, avg_score, high_score, price, n, accuracy, mtnpb])
+        mtnpb = mtnpb - 0.75
+
+    print(x)
+
+
+test_evolutionary_algorithm_with_changing_population_size(Data.SHOTONS, Selection.BEST, 0.85, 0.4, 100, 99, 400,
+                                                                   13.0)
